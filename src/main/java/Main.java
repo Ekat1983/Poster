9ef5223c18683851e2be7cc13ee4992e2ad4eb9f
ekat1983 <shumkovakatya@yandex.ru>
2022-07-18 22:35:25 +0300

    correction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,17 @@ public class Main {
     public static void main(String args) {
         PosterManager manager = new PosterManager();
 
-                manager.add("one");
-                manager.add("two");
-                manager.add("threе");
-                manager.add("four");
-                manager.add("five");
-                manager.add("six");
-                manager.add("seven");
-                manager.add("eight");
-                manager.add("nine");
-                manager.add("ten");
+        manager.add("one");
+        manager.add("two");
+        manager.add("threе");
+        manager.add("four");
+        manager.add("five");
+        manager.add("six");
+        manager.add("seven");
+        manager.add("eight");
+        manager.add("nine");
+        manager.add("ten");
 
 
-        }
+    }
 }
--- src/main/java/PosterManager.java
+++ src/main/java/PosterManager.java
@@ -2,25 +2,26 @@ public class PosterManager {
     private String[] movies = new String[0];
     private int limit;
 
-   public PosterManager() {//limit = 10
-      limit = 10;
+    public PosterManager() {//limit = 10
+        limit = 10;
     }
+
     public PosterManager(int limit) {
-       this.limit = limit;
+        this.limit = limit;
 
     }
 
     public void add(String poster) {
         String[] tmp = new String[movies.length + 1];
-        for (int i = 0; i< movies.length; i++) {
-            tmp[i] = movies [i];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
         }
         tmp[tmp.length - 1] = poster;
         movies = tmp;
 
     }
 
-   public String[] findAll() {
+    public String[] findAll() {
         return movies;
 
     }
@@ -29,7 +30,7 @@ public class PosterManager {
         String[] ans;
         if (limit < movies.length) {
             ans = new String[limit];
-        }else {
+        } else {
             ans = new String[movies.length];
         }
         for (int i = 0; i < ans.length; i++) {
